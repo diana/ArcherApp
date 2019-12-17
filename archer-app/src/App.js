974ff1cf7147137769a6a2519c2e1139d07b3198
diana-mm <dmmiller85@gmail.com>
2019-12-17 16:04:55 -0700

    finishes app for presentation

--- archer-app/src/App.js
+++ archer-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import Episodes from './components/Episodes';
+import './app/app.css'
 
 
 
@@ -8,15 +9,13 @@ class App extends Component {
   state = {
     isClicked: false,
     isLoggedIn: false,
-    episodes: [
-    {}
-    ]
+    episodes: []
   }
   
   componentDidMount () {
-    fetch('http://localhost:3000/')
-    .then(response => response.json)
-    .then(result => this.setState({episodes: result.episodes}))
+    fetch('http://localhost:3000/episodes')
+    .then(response => response.json())
+    .then(result => this.setState({episodes: result}))
   }
 
   createHeader = () => {
@@ -47,31 +46,49 @@ class App extends Component {
       <div className="modal">
           <header id="sign-in"> 
           <span onClick={this.setClickedLog}>&times;</span>
-          <button id='signup'>Sign Up!</button>
           <form id='login-form'>
               <label>Username: </label>
               <input id='name'></input>
           </form>
-          <button id='fake-button' onClick={this.setLogin}>submit</button>
+          <button id='fake-button' onClick={this.setLogin}>INSERT</button>
           </header>
       </div>
       </div> 
       )   
     }
 
+    closeForm = () => {
+      return(
+        <div>
+          
+        </div>
+      )
+    }
+
     setLogin = () => {
       return(
-        this.setState({isLoggedIn: !this.state.isLoggedIn})
+        this.setState({isLoggedIn: !this.state.isLoggedIn}),
+        this.setState({isClicked: !this.state.isClicked})
       )
     }
 
     createCard = () => {
-      return this.state.episodes.map(episode => 
+      return (
+        this.state.episodes.map(episode => 
         <Episodes 
-          key={episodes.id} 
-          episodes = {episode} 
+          key={episode.id} 
+          episode = {episode} 
           isLoggedIn = {this.state.isLoggedIn}
         />
+      ))
+    }
+
+    createH2 = () => {
+      return (
+        <div className="cards-container">
+          <h2>Archer Seasons 1-10 Episodes:</h2>
+          {this.createCard()}
+        </div>
       )
     }
   
@@ -80,13 +97,15 @@ class App extends Component {
     return (
 
       <div className="app">
-
+        
 
         <div>
-          {this.state.isClicked ? this.createLoginForm() : this.createHeader()}
+          {this.state.isClicked ? this.createLoginForm() :  null}
         </div>
         <div className="cards-container">
-          {this.state.isLoggedIn ? this.createCard() : null}
+          
+          {this.state.isLoggedIn ? this.createH2() : this.createHeader()}
+
          </div>
 
 
--- archer-app/src/components/Episodes.js
+++ archer-app/src/components/Episodes.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import EpisodeCard from './EpisodeCard'
+import React, { Component } from 'react'
 
-export default function Episodes (props) {
-
-    const showEpisodes = props.episodes.map((episode, i) =>(
-        <li key={i}><EpisodeCard episode={episode}/></li>
-    ))
-
-    return (
-        <div className="episodeContainer">
-            <ul className="episodeList">
-                {showEpisodes}
-            </ul>
-
-        </div>
-    )
-
-
+export default class Episodes extends Component {
+    render() {
+        const {title, season, season_episode, writer} = this.props.episode
+        return (
+            <div id='card'>
+                <h3 >
+                    {title}
+                </h3>
+                <p>
+                    Season: {season} , Episode: {season_episode}
+                </p>
+                <span>
+                    Written By: {writer}
+                </span>
+            </div>
+        )
+    }
 }
+
